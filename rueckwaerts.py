6a9d95e9dfcef6ec04e906a7ac92328bc064235c
Andreas Mergl <andreas.merg@web.de>
2016-06-14 11:29:01 +0200

    Fixing rueckwaerts calculation

--- rueckwaerts.py
+++ rueckwaerts.py
@@ -23,13 +23,13 @@ def newton(F,x0,eps=1e-5,alpha=1.05e-4):
         xalpha=dot(R,Fx)
         x-=xalpha
 
-        residuum2=linalg.norm(Fx)#-dot(dFx,xalpha))
+        residuum2=linalg.norm(Fx)
 	if residuum2-residuum > 0:
-            alpha/=10
+            alpha/=2
         if abs(residuum2/residuum) > 0.9:
-	    alpha/=5
-        elif residuum/residuum2 < 1e2:
-	    alpha*=5
+	    alpha/=2
+        elif residuum > residuum2:
+	    alpha*=4
 #        alpha=min(5e-5,residuum2/residuum)
 	residuum=residuum2
         #svd=linalg.svd(A,compute_uv=0)
@@ -41,15 +41,15 @@ def newton(F,x0,eps=1e-5,alpha=1.05e-4):
 def rueckwaerts(n,z,l_r):
     inp=array([[radians(float(i)/n * 360),vorwaerts(radians(float(i)/n * 360),x=z, l_r=l_r)] for i in range(n+1)],dtype=float64)
     F=lambda x: array([abs(vorwaerts(i[0],x,l_r)-i[1]) for i in inp],dtype=float64)
-    x0=array([30,30,radians(90),radians(90),90],dtype=float64)
-    #x0=array([1.7,6.,radians(120),radians(88),15],dtype=float64)
+    x0=array([10,30,radians(90),radians(90),90],dtype=float64)
 
-    for method in [ "CG","BFGS","Newton-CG","COBYLA","trust-ncg"]:
+    for method in [ "CG","BFGS","COBYLA",]:
         res=minimize(lambda x:linalg.norm(F(x),inf),x0,method=method)
-        print "Minimize:",linalg.norm(F(res.x),inf)
+        print "Minimize(%s): Residuum = %f, x = %s"%(method,linalg.norm(F(res.x),inf),res.x)
     res=fmin(lambda x:linalg.norm(F(x),inf),x0,disp=0)
-    print "Fmin:",linalg.norm(F(res),inf)
+    print "Fmin: Residuum = %f, x = %s"%(linalg.norm(F(res),inf),res)
     x,residuum=newton(F,x0, alpha=5e-4)
+    print x
 
     return residuum
     
@@ -68,12 +68,13 @@ if __name__ == "__main__":
     #x=[1,2,radians(90),radians(87),4]
     #l_r=0.5
     l_r = 0.3
-    l_e=10
-    h_m=29
-    phi0=radians(92.5)
-    psi=radians(90.5)
-    h_b=91
+    l_e=11
+    h_m=27.32
+    phi0=radians(92.111)
+    psi=radians(90.7)
+    h_b=89.8
     x=[l_e,h_m,phi0,psi,h_b]
+    print_x(x)
        
     residuum=rueckwaerts(n,x,l_r)
     print "X actually is",x
